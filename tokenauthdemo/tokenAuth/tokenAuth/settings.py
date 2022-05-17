184dcbc2239c518bf8a8a7fc4d3864ea51591551
Nitindrdarker <oct2001nitin@gmail.com>
2022-05-17 16:51:40 +0530

    minor changes in settings.py

--- tokenauthdemo/tokenAuth/tokenAuth/settings.py
+++ tokenauthdemo/tokenAuth/tokenAuth/settings.py
@@ -54,7 +54,14 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'tokenAuth.urls'
 
-
+REST_FRAMEWORK = {
+    'DEFAULT_PARSER_CLASSES':(
+        'rest_framework.parsers.JSONParser', 
+        ),
+    'DEFAULT_RENDERER_CLASSES':(
+        'rest_framework.renderers.JSONRenderer',
+        ),
+    }
 
 
 
